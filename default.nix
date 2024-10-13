a554c409fe0d8ebd1925cd80fe1652b7f866550f
Heitor Augusto <IAm.HeitorALN@proton.me>
2024-10-12 23:55:56 -0300

    feat: improve nix package configuration management (#5)

--- default.nix
+++ default.nix
@@ -1,11 +1,10 @@
 {
   lib,
   writeText,
+  runCommand,
+  jc,
   where-is-my-sddm-theme,
   /*
-    NOTE: Don't write a key that is already defined in the theme's configuration file.
-          The key will not be overwritten, and it will be defined twice in the configuration file.
-
     Here is how you can define extra settings for the theme and the flavor to use:
 
     environment.systemPackages = [
@@ -39,17 +38,16 @@ let
     "mocha"
   ];
 
-  themeContent = builtins.readFile "${theme}/themes/catppuccin-${flavor}.conf";
-  customThemeContent =
+  # Borrowed from catppuccin/nix
+  fromINI =
+    file:
     let
-      removeDuplicates =
-        string: builtins.concatStringsSep "\n" (lib.unique (lib.strings.splitString "\n" string));
+      json = runCommand "converted.json" { } ''${lib.getExe jc} --ini < ${file} > $out'';
     in
-    if settings == null then
-      themeContent
-    else
-      removeDuplicates (themeContent + lib.generators.toINI { } settings);
-  customTheme = writeText "theme.conf.user" customThemeContent;
+    builtins.fromJSON (builtins.readFile json);
+
+  themeContent = fromINI "${theme}/themes/catppuccin-${flavor}.conf";
+  customTheme = lib.recursiveUpdate themeContent (lib.optionalAttrs (settings != null) settings);
 in
 lib.throwIfNot (builtins.elem flavor validFlavors)
   "catppuccin-where-is-my-sddm-theme: flavor ${flavor} is not valid. Valid flavors are: ${builtins.concatStringsSep ", " validFlavors}"
@@ -57,27 +55,19 @@ lib.throwIfNot (builtins.elem flavor validFlavors)
   "catppuccin-where-is-my-sddm-theme: variant"
   validVariants
   variants
-  (where-is-my-sddm-theme.override { inherit variants; }).overrideAttrs
-  (
-    finalAttrs: oldAttrs: {
-      pname = "catppuccin-where-is-my-sddm-theme";
-      version = "1.0.0";
+  (where-is-my-sddm-theme.override {
+    inherit variants;
+    themeConfig = customTheme;
+  }).overrideAttrs
+  (oldAttrs: {
+    pname = "catppuccin-where-is-my-sddm-theme";
+    version = "1.0.0";
 
-      installPhase =
-        oldAttrs.installPhase
-        + lib.optionalString (builtins.elem "qt6" variants) ''
-          ln -sf ${customTheme} $out/share/sddm/themes/where_is_my_sddm_theme/theme.conf.user
-        ''
-        + lib.optionalString (builtins.elem "qt5" variants) ''
-          ln -sf ${customTheme} $out/share/sddm/themes/where_is_my_sddm_theme_qt5/theme.conf.user
-        '';
-
-      meta = {
-        description = "Soothing pastel theme for Where is my SDDM theme?";
-        homepage = "https://github.com/catppuccin/where-is-my-sddm-theme";
-        license = lib.licenses.mit;
-        maintainers = with lib.maintainers; [ HeitorAugustoLN ];
-        inherit (oldAttrs.meta) platforms;
-      };
-    }
-  )
+    meta = {
+      description = "Soothing pastel theme for Where is my SDDM theme?";
+      homepage = "https://github.com/catppuccin/where-is-my-sddm-theme";
+      license = lib.licenses.mit;
+      maintainers = with lib.maintainers; [ HeitorAugustoLN ];
+      inherit (oldAttrs.meta) platforms;
+    };
+  })
